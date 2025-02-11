18d89b39453f916f9d14f8b521a25be192f3191c
Parsa Estakhri <parsaestakhry03@gmail.com>
2025-02-11 13:19:16 +0330

    enhance Application class with comments and update startup message

--- src/main/java/com/example/parsa/tutorial_spring_app/Application.java
+++ src/main/java/com/example/parsa/tutorial_spring_app/Application.java
@@ -8,24 +8,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
+// @SpringBootApplication is a convenience annotation that adds all of the following:
+// - @Configuration: Tags the class as a source of bean definitions for the application context.
+// - @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings.
+// - @ComponentScan: Tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers.
+
 @SpringBootApplication
 public class Application {
+
+	// main method is used to launch the appliation using Spring Boot's
+	// SpringApplication.run() method.]
+	// application is a complete java application
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	// it will run on start up
 	@Bean
 	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
 		return _ -> {
 
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
+			System.out.println("application is running on localhost:8080");
 
 		};
 	}
